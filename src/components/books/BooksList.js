0b990b3d56b3b292a1ce92cc251cfb4630cdaeac
root <root@DESKTOP-QRLQ7Q8.localdomain>
2019-12-17 22:21:01 +0100

    elipse large title & apply css

--- src/components/books/BooksList.js
+++ src/components/books/BooksList.js
@@ -1,12 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./css/booksList.css";
+import EllipsisText from "react-ellipsis-text";
 
 const BooksList = ({ books }) => {
 	return (
 		<ul className="list-group">
 			{books.map(book => (
-				<li key={book.title} className="list-group-item">
-					{book.title}
+				<li key={book.id} className="list-group-item bookListItem">
+					<a
+						className="bookListItemTitle"
+						href={`/books/${book.slug}`}
+					>
+						<EllipsisText
+							className="bookListItemAuthor"
+							length={35}
+							text={book.title}
+							tooltip={book.title}
+						/>
+					</a>
+					<button
+						type="button"
+						className={`btn btn-outline-primary btn-sm bookListItemGenre genreColor${book.genre.replace(
+							" ",
+							""
+						)}`}
+					>
+						{book.genre}
+					</button>
+					<EllipsisText
+						className="bookListItemAuthor text-secondary"
+						text={book.author}
+						length={70}
+					/>
 				</li>
 			))}
 		</ul>
--- tools/mockData.js
+++ tools/mockData.js
@@ -4,6 +4,7 @@ const books = [
 		title: "Pol Colomo y sus amigos",
 		slug: "pol-colomo-y-sus-amigos",
 		authorId: 1,
+		author: "Jaco Baldrich",
 		genre: "Children",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -11,7 +12,8 @@ const books = [
 		id: 2,
 		title: "Vida y Azañas de Pol Colomo",
 		slug: "vida-y-azañas-de-pol-colomo",
-		authorId: 1,
+		authorId: 2,
+		author: "Pol Colomo",
 		genre: "Biography",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -20,6 +22,7 @@ const books = [
 		title: "Cuando Pol Colomo entra en tu vida",
 		slug: "cuando-pol-colomo-entra-en-tu-vida",
 		authorId: 1,
+		author: "Jaco Baldrich",
 		genre: "Philosophy",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -27,7 +30,8 @@ const books = [
 		id: 4,
 		title: "¿Por qué Pol Colomo y no Juan Palomo?",
 		slug: "por-que-pol-colomo-y-no-juan-palomo",
-		authorId: 1,
+		authorId: 3,
+		author: "Uri Ustrell",
 		genre: "Cooking",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -35,7 +39,8 @@ const books = [
 		id: 5,
 		title: "Pol Colomo, solamente",
 		slug: "pol-colomo-solamente",
-		authorId: 1,
+		authorId: 2,
+		author: "Pol Colomo",
 		genre: "Poetry",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -43,7 +48,8 @@ const books = [
 		id: 6,
 		title: "|*()|_ {()|_()|`´|(), el hacker",
 		slug: "pol-colomo-el-hacker",
-		authorId: 1,
+		authorId: 3,
+		author: "Uri Ustrell",
 		genre: "Software",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -52,6 +58,7 @@ const books = [
 		title: "Un nuevo caso para: Pol Colom(b)o",
 		slug: "un-nuevo-caso-para-pol-colombo",
 		authorId: 1,
+		author: "Jaco Baldrich",
 		genre: "Crime",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -60,6 +67,7 @@ const books = [
 		title: "¿Qué come Pol Colomo?",
 		slug: "que-come-pol-colomo",
 		authorId: 1,
+		author: "Jaco Baldrich",
 		genre: "Suspense",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -67,7 +75,8 @@ const books = [
 		id: 9,
 		title: "Conviértete en maestro del póker con Pol Colomo",
 		slug: "conviertete-en-maestro-del-poker-con-pol-colomo",
-		authorId: 1,
+		authorId: 2,
+		author: "Pol Colomo",
 		genre: "Self help",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	},
@@ -75,7 +84,8 @@ const books = [
 		id: 10,
 		title: "Pol Colomo Fundamentals",
 		slug: "pol-colomo-fundamentals",
-		authorId: 1,
+		authorId: 3,
+		author: "Uri Ustrell",
 		genre: "Encyclopedia",
 		photo: "https://https://polcolomo.com/assets/img/pol-home-leftblack.png"
 	}
